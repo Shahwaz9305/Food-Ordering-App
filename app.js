9a3f6d976e6879c6bf888667cac3aadb08d7ef32
Mohd_Shahwaz <mohdshahwaz9305@gmail.com>
2025-06-23 18:12:21 +0530

    Episode-2

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+    import React from 'react';
+    import ReactDOM from 'react-dom/client'
+
+    
     const heading = React.createElement('h1',{},"React Food Ordering App")
     const root = ReactDOM.createRoot(document.getElementById('root'));
     root.render(heading);
