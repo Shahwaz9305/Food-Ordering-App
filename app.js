f54ceb21bea76de0672a44dba8a109e5819a17b9
Mohd_Shahwaz <mohdshahwaz9305@gmail.com>
2025-06-24 14:27:23 +0530

    episode-3

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
-    import React from 'react';
-    import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-    
-    const heading = React.createElement('h1',{},"React Food Ordering App")
-    const root = ReactDOM.createRoot(document.getElementById('root'));
-    root.render(heading);
+const heading = React.createElement("h1", {}, "React Food Ordering App");
+const Title = () => {
+  return <h1>React Food Ordering App using jsx</h1>;
+};
+const FunctionalComponent = () => {
+  return (
+    <div>
+      <Title />
+      <h1>Functional Component.............</h1>
+    </div>
+  );
+};
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<FunctionalComponent/>);
